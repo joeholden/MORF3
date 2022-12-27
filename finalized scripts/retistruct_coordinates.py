093670bc907f7b07be9374f340deb27fff80b7d5
Joe Holden <joemattholden@gmail.com>
2022-12-26 23:47:54 -0600

    Add files via upload
    fixed axes error

--- finalized scripts/retistruct_coordinates.py
+++ finalized scripts/retistruct_coordinates.py
@@ -3,6 +3,7 @@ import os
 import math
 import scipy.io
 
+# Keep flip DV unchecked
 ANIMAL_NUMBER = "3237"
 SIDE = "right"
 MATLAB_PATH = r"C:\Users\joema\Desktop\RETISTRUCT_Working\3237 LE\right.mat".replace("\\", os.sep)
@@ -20,12 +21,36 @@ def excel_from_matlab(path, animal, side):
                                                                                                 'Positive Radians)']]
 
     if SIDE.lower() == 'right':
-        df['Theta Degrees in Left Eye Space'] = list(map(lambda x: x + 180, df['Longitude (Theta Positive Degrees)']))
+        df['Theta Degrees in Left Eye Space'] = list(map(lambda x: 180 - x, df['Longitude (Theta Positive Degrees)']))
         df['Theta Degrees in Left Eye Space'] = [i - 360 if i > 360 else i for i in df['Theta Degrees in Left Eye Space']]
+        df['Theta Degrees in Left Eye Space'] = [i + 360 if i < 0 else i for i in
+                                                 df['Theta Degrees in Left Eye Space']]
     elif SIDE.lower() == 'left':
         df['Theta Degrees in Left Eye Space'] = df['Longitude (Theta Positive Degrees)']
     else:
         print('Spelling Error')
+
+    def region(angle):
+        if 0 < angle < 90:
+            return 'DT'
+        elif 90 < angle < 180:
+            return 'DN'
+        elif 180 < angle < 270:
+            return 'VN'
+        elif 270 < angle < 360:
+            return 'VT'
+        elif angle == 0 or angle == 360:
+            return 'T'
+        elif angle == 90:
+            return 'D'
+        elif angle == 180:
+            return 'N'
+        elif angle == 270:
+            return 'V'
+        else:
+            return 'ERROR'
+
+    df['Region'] = list(map(region, df['Theta Degrees in Left Eye Space']))
 
     df.to_excel(f'{ANIMAL_NUMBER}_{SIDE}_RETISTRUCT.xlsx')
 
